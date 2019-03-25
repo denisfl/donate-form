8d5d89f8da26b0f3d0dfe943f0db332af1f4bdf6
Denis FL <delchyve@gmail.com>
2019-03-25 21:00:24 +0400

    Init project

---
+++ README.md
@@ -0,0 +1,24 @@
+## Installation
+
+1. Clone this repository
+2. Install all dependencies with `npm`
+```bash
+npm install
+```
+
+## Usage
+
+### Development
+To run development server on localhost (you can choose port of your choice in the config-file)
+
+```bash
+npm run dev
+```
+
+### Production
+
+To create a production ready build that is minified and properly packed:
+
+```bash
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    app: './app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader!sass-loader",
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env']
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 3000,   //Tell dev-server which port to run
+    open: false,   // to open the local server in browser
+    contentBase: path.resolve(__dirname, 'dist') //serve from 'dist' folder
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']), //cleans the dist folder
+    new ExtractTextPlugin("styles/app.css"), //etracts css to dist/css/styles.css
+    new HtmlWebpackPlugin({
+      template: 'index.ejs'
+    })
+  ]
+};
+
+module.exports = config;
